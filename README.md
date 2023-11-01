c0cea7808a8c9addb52fd7ae4737d80a1a405b4d
MickaelDev <77359029+SrMickael@users.noreply.github.com>
2023-11-01 02:17:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# createEditBox - Função de Caixa de Edição Personalizada em Lua
+
+A função `createEditBox` é uma ferramenta poderosa para criar caixas de edição personalizadas em Lua, com funcionalidades de inserção de texto, seleção, desenho na tela e manipulação de eventos. Essa função é especialmente útil para criar interfaces de usuário interativas em jogos ou aplicativos.
+
+## Como Usar
+
+Para criar uma caixa de edição personalizada, siga estas etapas:
+
+1. Inclua a função `createEditBox` em seu projeto Lua.
+
+2. Chame a função `createEditBox` e forneça os parâmetros necessários:
+
+```lua
+local editBox = createEditBox(x, y, width, height, text, maxLength, font, colors, fieldType, alpha)
+```
+
+
+- Personalize os parâmetros de acordo com suas necessidades:
+  - `x` e `y`: Defina as coordenadas (x, y) da caixa de edição.
+  - `width` e `height`: Especifique a largura e altura da caixa de edição.
+  - `text`: Configure o texto inicial da caixa de edição.
+  - `maxLength`: Limite a quantidade máxima de texto que pode ser inserida.
+  - `font`: Escolha a fonte e o tamanho do texto.
+  - `colors`: Defina cores para o texto, borda e seleção.
+  - `fieldType`: Escolha entre "text" para texto normal ou "password" para ocultar o texto.
+  - `alpha`: Controle a transparência da caixa de edição.
+
+- Para manipular eventos, use os métodos disponíveis:
+  - `addEventHandler(eventName, handlerFunction)`: Adicione manipuladores de eventos para responder a ações do usuário, como cliques do mouse e teclas pressionadas.
+  - `destroy()`: Remova todos os manipuladores de eventos quando a caixa de edição não for mais necessária.
+
+- Use o método `draw` para desenhar a caixa de edição na tela. É possível especificar um nível de transparência opcional:
+```lua
+editBox:draw(255)
+```
+
+
+- Manipule eventos, como cliques do mouse e entradas de teclado, com os métodos específicos fornecidos na função `createEditBox`.
+
+- Integre a caixa de edição personalizada em seu projeto Lua e utilize-a para criar interfaces de usuário interativas.
+
+## Exemplo de Uso
+
+Aqui está um exemplo simples de como criar e usar a caixa de edição personalizada:
+
+```lua
+local editBox = createEditBox(100, 100, 200, 30, "Digite aqui", 10, "default", {textColor = {255, 255, 255, 1}, borderColor = {10, 10, 10, 1}}, "text")
+-- Desenhe a caixa de edição na tela
+addEventHandler("onClientRender", root, function()
+    editBox:draw(255)
+end)
+```
+
+
+
+## Contribuições
+
+Sinta-se à vontade para contribuir com melhorias ou correções para esta função `createEditBox`. Caso encontre problemas ou tenha sugestões, crie uma "Issue" neste repositório.
+
+## Licença
+
+Este projeto é licenciado sob a [Licença MIT](LICENSE). Você é livre para usar, modificar e distribuir este código de acordo com os termos da licença.
+
+## Autor
+
+Mickael junio - *Discord*: mickaeljr
+
+## Agradecimentos
+
+Agradeça a outros colaboradores ou projetos que tenham inspirado ou ajudado na criação desta função.
---
+++ client.lua
@@ -0,0 +1,166 @@
+function createEditBox(x, y, width, height, text, maxLength, font, colors, fieldType)
+    local editBox = {
+        x = x or 100,
+        y = y or 100,
+        width = width or 200,
+        height = height or 30,
+        text = "",
+        textLength = text or "",
+        maxLength = maxLength or 50,
+        font = font or 'default',
+        textColor = colors['textColor'] or {255, 255, 255, 1},
+        borderColor = colors['borderColor'] or {10, 10, 10, 1},
+        selectedColor = {0, 119, 192},
+        cursorIndex = 0,
+        selectionStart = 0,
+        selectionEnd = 0,
+        isSelecting = false,
+        isSelected = false,
+        fieldType = fieldType or "text",
+        eventHandlers = {}, -- Uma tabela para armazenar os manipuladores de eventos
+    }
+    
+    function editBox:addEventHandler(eventName, handlerFunction)
+        local eventHandler = addEventHandler(eventName, root, handlerFunction)
+        table.insert(self.eventHandlers, {eventName, root, eventHandler})
+    end
+
+    function editBox:destroy()
+        -- Remova todos os manipuladores de evento da tabela eventHandlers
+        for _, eventHandler in ipairs(self.eventHandlers) do
+            removeEventHandler(eventHandler[1], eventHandler[2], eventHandler[3])
+        end
+    end
+
+    -- Função para desenhar a caixa de edição
+    function editBox:draw(alpha)
+        local alpha = alpha or 255
+        local textToDraw = self.text
+
+        dxDrawRectangle(self.x, self.y, self.width, self.height, tocolor(self.borderColor[1], self.borderColor[2], self.borderColor[3], alpha*self.borderColor[4]))
+
+        if #textToDraw > 0 and self.fieldType == "password" then
+            -- Substitua o texto por asteriscos para campos de senha
+            local passwordText = string.rep("*", #textToDraw)
+            textToDraw = passwordText
+        end
+
+        local WText = dxGetTextWidth(string.sub(textToDraw, 1, self.cursorIndex), 1, self.font)
+        local waw = dxGetTextWidth(textToDraw, 1, self.font)
+        local _, textHeight = dxGetTextSize(textToDraw, waw, 1, self.font)
+        
+
+        if self.isSelected then
+            if self.isSelecting then
+                textToDraw = string.sub(textToDraw, self.selectionStart + 1, self.selectionEnd)
+                dxDrawRectangle(self.x + 5, self.y, (waw >= self.width and self.width - 5 or waw), self.height, tocolor(self.selectedColor[1], self.selectedColor[2], self.selectedColor[3], alpha*0.70))
+            end
+            dxDrawRectangle((self.x + (WText >= self.width and self.width - 5 or WText + 5)), self.y+(textHeight/2), 1, textHeight, tocolor(255, 255, 255, math.abs(math.sin(getTickCount() / 500) * 255)))
+        else
+            textToDraw = (#textToDraw > 0 and textToDraw or self.textLength)
+        end
+
+        dxDrawText(textToDraw, self.x + 5, self.y, self.x + self.width - 5, self.y + self.height, tocolor(self.textColor[1], self.textColor[2], self.textColor[3], alpha*self.textColor[4]), 1, self.font, (WText >= self.width - 5 and "right" or "left"), "center", true)
+    end
+
+
+    function editBox:onClientPaste(text)
+        local clipboardText = text
+        local texts = string.sub(self.text, 1, self.selectionStart) .. clipboardText .. string.sub(self.text, self.selectionEnd + 1)
+        
+        if self.isSelected and clipboardText and #texts <= self.maxLength then
+            if self.isSelecting then
+                self.text = string.sub(self.text, 1, self.selectionStart) .. clipboardText .. string.sub(self.text, self.selectionEnd + 1)
+                self.cursorIndex = self.selectionStart + #clipboardText
+                self.isSelecting = false
+            else
+                self.text = string.sub(self.text, 1, self.cursorIndex) .. clipboardText .. string.sub(self.text, self.cursorIndex + 1)
+                self.cursorIndex = self.cursorIndex + #clipboardText
+            end
+        end
+    end
+
+
+    -- Função para manipular a entrada de texto
+    function editBox:handleInput(key, state)
+        if (state) then
+            if key == "backspace" then
+                if self.isSelected then
+                    if self.isSelecting then
+                        self.text = string.sub(self.text, 1, self.selectionStart) .. string.sub(self.text, self.selectionEnd + 1)
+                        self.cursorIndex = self.selectionStart
+                        self.isSelecting = false
+                    elseif self.cursorIndex > 0 then
+                        self.text = string.sub(self.text, 1, self.cursorIndex - 1) .. string.sub(self.text, self.cursorIndex + 1)
+                        self.cursorIndex = self.cursorIndex - 1
+                    end
+                end   
+            elseif key == "c" and getKeyState("lctrl") then
+                if self.isSelecting then
+                    local selectedText = string.sub(self.text, self.selectionStart + 1, self.selectionEnd)
+                    setClipboard(selectedText)
+                end
+            elseif key == "a" and getKeyState("lctrl") then
+                self.selectionStart = 0
+                self.selectionEnd = #self.text
+                self.isSelecting = true
+            end
+        end
+    end
+
+    -- Função para lidar com a digitação de caracteres
+    function editBox:handleCharacter(char)
+        if self.isSelected and #self.text < self.maxLength then
+            if self.isSelecting then
+                self.text = string.sub(self.text, 1, self.selectionStart) .. char .. string.sub(self.text, self.selectionEnd + 1)
+                self.cursorIndex = self.selectionStart + 1
+                self.isSelecting = false
+            else
+                self.text = string.sub(self.text, 1, self.cursorIndex) .. char .. string.sub(self.text, self.cursorIndex + 1)
+                self.cursorIndex = self.cursorIndex + 1
+            end
+        end
+    end
+
+    -- Manipulação de eventos
+    function editBox:onClientClick(button, state, mouseX, mouseY)
+        if button == "left" and state == "down" then
+            if mouseX >= self.x and mouseX <= self.x + self.width and mouseY >= self.y and mouseY <= self.y + self.height then
+                self.isSelected = true
+                guiSetInputMode("no_binds")
+                local textWidth = {dxGetTextWidth(self.text, self.fontSize, self.font)}
+                local mouseOffset = mouseX - self.x
+                for i = 0, #self.text do
+                    local subText = string.sub(self.text, 1, i)
+                    local subTextWidth = dxGetTextWidth(subText, self.fontSize, self.font)
+                    if subTextWidth >= mouseOffset or i == #self.text then
+                        self.cursorIndex = i
+                        break
+                    end
+                end
+            else
+                self.isSelected = false
+                guiSetInputMode("allow_binds")
+                self.isSelecting = false
+            end
+        end
+    end
+
+    editBox:addEventHandler("onClientPaste", function(text)
+        editBox:onClientPaste(text)
+    end)
+
+    editBox:addEventHandler("onClientClick", function(button, state, mouseX, mouseY)
+        editBox:onClientClick(button, state, mouseX, mouseY)
+    end)
+
+    editBox:addEventHandler("onClientCharacter", function(char)
+        editBox:handleCharacter(char)
+    end)
+
+    editBox:addEventHandler("onClientKey", function(key, state)
+        editBox:handleInput(key, state)
+    end)
+
+    return editBox
+end
